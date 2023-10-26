6e18ed210bad6bcb44ef08a76371390e443a0df8
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-10-26 12:44:46 +0200

    Navbar

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+export default function Navbar() {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashbord", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug size={"1.5rem"} color={"purple"} />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            href={link.href}
+            key={link.href}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors font-bold": true,
+            })}>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+}
